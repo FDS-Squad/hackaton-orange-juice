0b504f7dd76b251c8975476c61c7eb92907befb9
Danrley Senegalha Pires <dansenpir.tech@outlook.com>
2023-09-23 16:46:00 -0300

    feat: add server

---
+++ apps/api/src/server.ts
@@ -0,0 +1,7 @@
+import './config/module-alias';
+import app from '@/config/app';
+import env from '@/config/env';
+
+app.listen(env.port, () => {
+  console.log(`Server running at http://localhost:${env.port}`);
+});
