ef3b64da046e8c5870fa2340b8b86d3eb63af0fa
Angela Caldas <aes.caldas@gmail.com>
2023-09-23 16:46:43 -0300

    chore: add event-details responsivity

--- apps/web/src/views/EventDetailsView.vue
+++ apps/web/src/views/EventDetailsView.vue
@@ -20,32 +20,36 @@ const shareWA = () => {
 
 <template>
   <main>
-    <section class="event-details wrapper">
-      <div class="card p-4 w-100" style="width: 18rem">
+    <section
+      class="event-details wrapper row justify-content-center mt-lg-4 gap-5">
+      <div class="card col-12 col-lg-6 p-4 mb-4 w-100">
         <img src="..." class="card-img-top" alt="..." />
         <div class="card-body pb-0">
           <h5 class="card-title text-center">Lorem ipsum dolor sit.</h5>
-          <p class="card-text text-center">08 out 2023</p>
-          <p class="card-text text-center">online</p>
-          <p class="card-text text-center">gratuito</p>
+          <p class="card-text text-center mb-0">08 out 2023</p>
+          <p class="card-text text-center mb-0">online</p>
+          <p class="card-text text-center mb-0">gratuito</p>
         </div>
       </div>
-      <div class="description">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit
-        facilis necessitatibus, eos non quaerat nostrum debitis? Nobis odit, rem
-        cum quibusdam iste perferendis voluptate nostrum obcaecati quas voluptas
-        similique asperiores nesciunt sint repellendus iusto, praesentium,
-        necessitatibus quam perspiciatis et eum quod cumque? Adipisci laboriosam
-        eum dicta at ab quidem repellat.
-      </div>
-      <div class="actions">
-        <button class="btn btn-primary btn-sm" @click="copyLink">
-          Copiar link
-        </button>
-        <button class="btn btn-primary btn-sm" @click="shareWA">
-          Compartilhar WP
-        </button>
-        <button class="btn btn-primary btn-sm">Visitar site</button>
+      <div class="col-12 col-lg-6">
+        <div class="description text-center pb-4 pb-lg-5">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit
+          facilis necessitatibus, eos non quaerat nostrum debitis? Nobis odit,
+          rem cum quibusdam iste perferendis voluptate nostrum obcaecati quas
+          voluptas similique asperiores nesciunt sint repellendus iusto,
+          praesentium, necessitatibus quam perspiciatis et eum quod cumque?
+          Adipisci laboriosam eum dicta at ab quidem repellat.
+        </div>
+        <div
+          class="actions d-flex justify-content-center align-items-end pt-lg-5 gap-3">
+          <button class="btn btn-primary btn-sm" @click="copyLink">
+            Copiar link
+          </button>
+          <button class="btn btn-primary btn-sm" @click="shareWA">
+            Compartilhar WP
+          </button>
+          <button class="btn btn-primary btn-sm">Visitar site</button>
+        </div>
       </div>
     </section>
   </main>
@@ -53,19 +57,18 @@ const shareWA = () => {
 
 <style scoped>
 .event-details {
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-
   img {
     background: #ccc;
     width: 100%;
     height: 180px;
   }
+}
 
-  .actions {
-    display: flex;
-    gap: 1rem;
+@media screen and (min-width: 1200px) {
+  .event-details {
+    .card {
+      width: 500px !important;
+    }
   }
 }
 </style>
