1ad79b3d2acc767075c6f2abf0696ff4f00b5d6b
Danrley Senegalha Pires <dansenpir.tech@outlook.com>
2023-09-24 17:18:43 -0300

    test: events list service should return a promise of type IEvent

---
+++ apps/api/tests/modules/services/events-list-service.spec.ts
@@ -0,0 +1,27 @@
+import { EventsRepository } from '../../../src/modules/events/repository/events-repository';
+import { EventsListService } from '../../../src/modules/events/services/events-list-service';
+
+describe('EventsListService', () => {
+  it('should return a promise of type IEvent', async () => {
+    const repository = new EventsRepository();
+    const sut = new EventsListService(repository);
+
+    const response = await sut.execute();
+    expect(Array.isArray(response)).toBe(true);
+    response.forEach((event) => {
+      expect(typeof event.id).toBe('string');
+      expect(typeof event.title).toBe('string');
+      expect(typeof event.description).toBe('string');
+      expect(typeof event.eventLink).toBe('string');
+      expect(typeof event.city).toBe('string');
+      expect(typeof event.state).toBe('string');
+      expect(typeof event.locationType).toBe('string');
+      expect(typeof event.price).toBe('string');
+      expect(typeof event.category).toBe('string');
+      expect(typeof event.startDate).toBe('object');
+      expect(typeof event.endDate).toBe('object');
+      expect(typeof event.createdAt).toBe('object');
+      expect(typeof event.updatedAt).toBe('object');
+    });
+  });
+});
