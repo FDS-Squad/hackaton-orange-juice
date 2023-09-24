60e8e717cc27223bc5efc41eb142bb38f3535870
Danrley Senegalha Pires <dansenpir.tech@outlook.com>
2023-09-24 17:54:32 -0300

    refactor: move fake event creation to a factory method

--- apps/api/tests/modules/events/repository/events-repository.spec.ts
+++ apps/api/tests/modules/events/repository/events-repository.spec.ts
@@ -1,47 +1,31 @@
+const makeFakeEvent = () => ({
+  id: 'any_id',
+  title: 'any_title',
+  description: 'any_description',
+  eventLink: 'any_link',
+  imageUrl: 'any_url',
+  city: 'any_city',
+  state: 'any_state',
+  locationType: 'any_location',
+  category: 'any_category',
+  startDate: new Date('2023-09-22'),
+  endDate: new Date('2023-09-24'),
+  createdAt: new Date('2023-09-22'),
+  updatedAt: new Date('2023-09-24'),
+});
+
 import { EventsRepository } from '../../../../src/modules/events/repository/events-repository';
 
 jest.mock('../../../../src/utils/prisma', () => ({
   events: {
-    findMany: jest.fn().mockResolvedValue([
-      {
-        id: 'any_id',
-        title: 'any_title',
-        description: 'any_description',
-        eventLink: 'any_link',
-        imageUrl: 'any_url',
-        city: 'any_city',
-        state: 'any_state',
-        locationType: 'any_location',
-        category: 'any_category',
-        startDate: new Date('2023-09-22'),
-        endDate: new Date('2023-09-24'),
-        createdAt: new Date('2023-09-22'),
-        updatedAt: new Date('2023-09-24'),
-      },
-    ]),
+    findMany: jest.fn().mockResolvedValue([makeFakeEvent()]),
   },
 }));
 
 describe('EventsRepository', () => {
-  it('should return all events', async () => {
+  it('should list events', async () => {
     const sut = new EventsRepository();
     const events = await sut.listEvents();
-    expect(events).toEqual([
-      {
-        id: 'any_id',
-        title: 'any_title',
-        description: 'any_description',
-        eventLink: 'any_link',
-        imageUrl: 'any_url',
-        city: 'any_city',
-        state: 'any_state',
-        locationType: 'any_location',
-        category: 'any_category',
-        startDate: new Date('2023-09-22'),
-        endDate: new Date('2023-09-24'),
-        createdAt: new Date('2023-09-22'),
-        updatedAt: new Date('2023-09-24'),
-      },
-    ]);
+    expect(events).toEqual([makeFakeEvent()]);
   });
 });
