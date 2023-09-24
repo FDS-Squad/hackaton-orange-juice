c3513abe922e7e657cc85e0ae568f5c319cc2479
Angela Caldas <aes.caldas@gmail.com>
2023-09-24 11:26:57 -0300

    chore: refactor routes object to load page title dynamically

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,25 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from 'vue-router';
+
+import PageBody from '../structure/PageBody.vue';
 import HomeView from '../views/HomeView.vue';
 
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: 'home',
+    component: PageBody,
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: HomeView,
+        meta: { title: 'Tech42 | Home' },
+      },
+      {
+        path: '/event-details',
+        name: 'event-details',
+        component: () => import('../views/EventDetailsView.vue'),
+        meta: { title: 'Tech42 | Detalhes' },
+      },
+      {
+        path: '/admin',
+        name: 'admin',
+        component: () => import('../views/AdminView.vue'),
+        meta: { title: 'Tech42 | Admin' },
+      },
+    ],
+  },
+  // {
+  //   path: '/unauthorized',
+  //   name: 'Unauthorized',
+  //   component: NotFound,
+  //   meta: { title: 'Não autorizado' },
+  // },
+  // {
+  //   path: '/:pathMatch(.*)*',
+  //   component: NotFound,
+  //   meta: { title: 'Página não encontrada' },
+  // },
+];
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/event-details',
-      name: 'event-details',
-      component: () => import('../views/EventDetailsView.vue'),
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: () => import('../views/AdminView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(from) {
+    if (!from.meta.doNotSlide) {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
